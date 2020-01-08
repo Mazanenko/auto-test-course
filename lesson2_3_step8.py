8e8f3b9990c1e9c2cfea9427d6ce96dcfd852b0b
Mazanenko <mazanenko@inbox.ru>
2020-01-08 20:14:53 +0300

    code corrected for verification

--- lesson2_3_step8.py
+++ lesson2_3_step8.py
@@ -7,18 +7,18 @@ import math
 
 browser = webdriver.Chrome()
 try:
+    # переходим по ссылке
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
-    # ждём снижения цены
+    # ждём снижения цены до 100
     WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.ID, "price"), "100")
          )
 
-    # push 'Book'
+    # жмём кнопку 'Book'
     button = browser.find_element_by_css_selector("#book")
     button.click()
 
-    # Решаем капчу
     # Уравнение
     def calc(x):
         return str(math.log(abs(12*math.sin(int(x)))))
